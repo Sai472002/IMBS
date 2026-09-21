cf73f10ed6327e1fb1fe240f9a88c6f5fa0f0298
Sai472002 <saidhasun0407@gmail.com>
2026-09-21 22:31:56 +0530

    added shadcn library

--- imbs_frontend/src/AUTHENTICATION/LOGIN/Login.tsx
+++ imbs_frontend/src/AUTHENTICATION/LOGIN/Login.tsx
@@ -1,9 +1,9 @@
-import Bg from "../ASSETS/IMAGES/login-background.png"
-
+import Bg from "../../ASSETS/IMAGES/login-background.png"
+import { Button } from "@/components/ui/button"
 const bgImage = <img src={Bg} className="size-full absolute" />
 
 function Login() {
-    return <>{bgImage} <div className="flex items-center justify-center size-full"><div className="p-4 rounded-md bg-primarylight1 size-80 z-50"><p>Welcome</p></div></div></>
+    return <>{bgImage} <div className="flex items-center justify-center size-full"><div className="p-4 rounded-md bg-primarylight1 size-80 z-50"><p>Welcome<Button>Ajima</Button></p></div></div></>
 }
 
 export default Login
